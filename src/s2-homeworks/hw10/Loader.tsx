4d6b69f08acbb9fefb411ad97ace15fb702d21cb
filatow <not-found@inbox.ru>
2025-01-15 18:06:26 +0300

    Finished homework 4.2

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,8 @@
 import s from './Loader.module.css'
+import loader from '../../assets/ellipse.png'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+  <div className={s.loader}>
+    <img src={loader} alt="loader"/>
+  </div>
+)
